3b60873d9d1595723c9e3dde96a1a0c4d1a124c0
sthefany <serrao.sthefany@icloud.com>
2023-09-01 13:01:01 -0300

    meu projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+Scanner scanner = new Scanner(System.in);
+
+        // Solicitar informações ao usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Número da Conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o Saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Fechar o scanner
+        scanner.close();
+
+        // Exibir mensagem com informações inseridas
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
-}
+
+    }
+
